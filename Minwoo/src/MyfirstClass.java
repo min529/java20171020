179ea992e4c21123c885005b59761db5df4eef1a
Administrator <Administrator@WIN-HLQT4LG0R7I>
2017-10-20 11:26:24 +0900

    GitTest 프로젝트 커밋

---
+++ Minwoo/src/MyfirstClass.java
@@ -0,0 +1,16 @@
+
+public class MyfirstClass {
+	public static void main(String[] args) {
+		
+		
+		int i = 1;
+		short s = 1;
+		byte b = 1;
+		long l = 1L;
+		
+		float f = 1.1F;
+		double d = 1.1;
+		System.out.println(Long.MAX_VALUE);
+	}
+
+}
